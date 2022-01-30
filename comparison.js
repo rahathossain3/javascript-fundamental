8f059da7f7cbd76af0d022027925c9aee6795b2e
rahathossain3 <rahathossain210@gmail.com>
2022-01-30 17:02:08 +0600

    conditional add

---
+++ comparison.js
@@ -0,0 +1,33 @@
+// getar_then ৫ > ৬ থেকে বড় নাকি বের করার জন্য > ব্যবহার করা হয়।
+// ----ডানপাশ থেকে বামপাশ বড় নাকি।
+// ---ডানপাশের সাথে বামপাশের তুলনা করে।
+// console.log(5 > 6);
+
+// less_then ৫ < ৬ থেকে ছোট নাকি বের করার জন্য < ব্যবহার করা হয়।
+// ------------2য় থেকে < 1ম ছোট নাকি-----
+// ----ডানপাশ থেকে বামপাশ ছোট নাকি।
+// console.log(5 < 6);
+
+//  সমান সমান বের করার জন্য == ব্যবহার করা হয়।
+// console.log(5 == 6);
+// console.log(6 == 6);
+
+// সমান নয় বা অসমান/ বিসম বের করার জন্য != ্ ব্যবহার করা হয়।
+// console.log(5 != 6);
+// console.log(6 != 6);
+
+
+var myLove = 99;
+var yourLove = 100;
+
+
+// console.log(myLove > yourLove);
+// console.log(myLove == yourLove);
+// console.log(myLove < yourLove);
+// console.log(myLove != yourLove);
+
+// && দ্বারা উভয় শর্ত পূরন করা লাগবে তা বুজায়
+console.log(studyGood && goodParson);
+
+// ||  দ্বারা যেকোন একটি শর্ত পূরন করা লাগবে তা বুজায়
+console.log(isBanker || goodRestul);
---
+++ conditional.js
@@ -0,0 +1,46 @@
+// var isFoodReady = true;
+var isFoodReady = false;
+// যদি food খাবার রেডি হয় তা্ইলে  আমি খামু
+// if food is resdy  i will eat
+// if food is ready == true
+
+// if (condition) {
+
+
+//  }
+
+if (isFoodReady == true) {
+    console.log('mama !! I will eat now')
+}
+
+
+var iphonePrice = 70000;
+var myBudget = 109500;
+// iphonPrice যদি myBudget এর থেকে কম হলে 
+
+// if (iphonePrice < myBudget) {
+//     console.log('Iphone diya pic tule futani Marbo!!')
+// }
+
+// if (iphonePrice > myBudget) {
+//     console.log('My Shaomi is the best')
+// }
+
+// ----- if else function--------
+
+// if (Condition) {
+//     //
+// }
+// clse{
+
+// }
+
+var chickenPrice = 180;
+var myMoney = 850;
+
+if (chickenPrice < myMoney) {
+    console.log('Yes! murgir Ran khamu ar Haddi chabamu');
+}
+else {
+    console.log('Smashed potato whit lentil shop');
+}
