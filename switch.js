745671337ae19392604a42d10fbdff861c568b1d
rahathossain3 <rahathossain210@gmail.com>
2022-02-03 16:28:38 +0600

    switch  & practice challenges

---
+++ switch.js
@@ -0,0 +1,23 @@
+var color = 'yellow';
+if (color == 'blue') {
+    console.log('color is blue');
+}
+else if (color == 'red') {
+    console.log('color is red');
+}
+else if (color == 'green') {
+    console.log('color is green');
+}
+else if (color == 'white') {
+    console.log('color is white');
+}
+else if (color == "magenta") {
+    console.log('color is Magenta');
+}
+else if (color == 'yellow') {
+    console.log('color is Yellow');
+}
+else {
+    console.log('color is black');
+}
+
